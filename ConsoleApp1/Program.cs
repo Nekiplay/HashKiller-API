108f20379c5cc247f72c825ff9d388c17f7a9dcf
Neki_play <neci.03@mail.ru>
2021-06-18 20:28:27 +0500

    Example add

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.Write("Напишите хеш: ");
+            var hash = Console.ReadLine();
             HashKiller_API.HashKiller HashKiller = new HashKiller_API.HashKiller();
-            var result = HashKiller.Get("$SHA$1881e3a008fafebc$b172d621b71cb6b2858043b6d83b231c43df65009326722afae1431a0a6e3022");
+            var result = HashKiller.Get(hash);
             Console.WriteLine("Пароль: " + result.Password);
             Console.ReadLine();
         }
