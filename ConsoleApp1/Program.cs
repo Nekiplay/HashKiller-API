ddf22982d5aa9d6a7df8b66c8aae11cec2b4c4ba
Neki_play <neci.03@mail.ru>
2021-06-18 20:22:48 +0500

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashKiller_API.HashKiller HashKiller = new HashKiller_API.HashKiller();
+            var result = HashKiller.Get("$SHA$1881e3a008fafebc$b172d621b71cb6b2858043b6d83b231c43df65009326722afae1431a0a6e3022");
+            Console.WriteLine("Пароль: " + result.Password);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HashKiller API/API.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Edge.SeleniumTools;
+using System;
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace HashKiller_API
+{
+    public class HashKiller
+    {
+        public HashKillerResult Get(string hash)
+        {
+            EdgeDriverService driverService = EdgeDriverService.CreateChromiumService();
+            EdgeOptions options = new EdgeOptions();
+            driverService.HideCommandPromptWindow = true;
+            options.UseChromium = true;
+            options.AddArgument("headless");
+            options.AddArgument("disable-gpu");
+            EdgeDriver driver = new EdgeDriver(driverService, options, new TimeSpan(0, 2, 0));
+            try
+            {
+                driver.Navigate().GoToUrl("https://hashkiller.ru");
+                Thread.Sleep(6550);
+                var hashelement = driver.FindElementByXPath("//input[@id='HForm']");
+                hashelement.SendKeys(hash);
+                string cap = Regex.Match(driver.PageSource, "Решите пример: (.*) =").Groups[1].Value;
+                var ansver = Calculator.Calc(cap).ToString();
+                var captchaelement = driver.FindElementByXPath("//input[@placeholder='Ответ числом']");
+                captchaelement.SendKeys(Convert.ToInt32(ansver).ToString());
+                var buttonelemt = driver.FindElementByXPath("//button[@id='submit-button']");
+                buttonelemt.Click();
+                HashKillerResult result = new HashKillerResult();
+                result.Password = driver.FindElementByXPath("//td[@class='center']//input[@name='hash']").GetAttribute("value");
+                result.Hash = hash;
+                driver.Quit();
+                return result;
+            }
+            catch
+            {
+                HashKillerResult result = new HashKillerResult();
+                result.Password = "";
+                result.Hash = "";
+                driver.Quit();
+                return result;
+            }
+
+        }
+        public static class Calculator
+        {
+            // Создадим статический экземпляр DataTable, чтобы каждый раз не инициализировать его заново
+            private static DataTable Table { get; } = new DataTable();
+
+            // Наш метод подсчета
+            // Добавьте отлов ошибок по вкусу)
+            public static double Calc(string Expression) => Convert.ToDouble(Table.Compute(Expression, string.Empty));
+        }
+    }
+
+    public class HashKillerResult
+    {
+        public string Hash;        
+        public string Password;
+    }
+}
